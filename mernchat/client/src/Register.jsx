e1c76b732dea75980bbb06d0c0b726cc856dc10e
Ronit Wilson <ronit.study@gmail.com>
2024-02-25 22:22:27 +0530

    start with register page

---
+++ mernchat/client/src/Register.jsx
@@ -0,0 +1,8 @@
+export default function Register() {
+    return(
+        <div className="bg-blue-50 h-screen" >
+            <input type="text" placeholder="Username" className="block" />
+            <input type="password" placeholder="Password" className="block"/>
+        </div>
+    )
+}
