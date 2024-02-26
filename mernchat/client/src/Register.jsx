95d834a1025b62853654a9230fe0a73e77753d34
Ronit Wilson <ronit.study@gmail.com>
2024-02-27 00:09:55 +0530

    update ui

--- mernchat/client/src/Register.jsx
+++ mernchat/client/src/Register.jsx
@@ -1,8 +1,11 @@
 export default function Register() {
     return(
-        <div className="bg-blue-50 h-screen" >
-            <input type="text" placeholder="Username" className="block" />
-            <input type="password" placeholder="Password" className="block"/>
+        <div className="bg-blue-50 h-screen flex items-center" >
+            <form className="w-64 mx-auto">
+            <input type="text" placeholder="Username" className="block w-full p-2 m-2 border" />
+            <input type="password" placeholder="Password" className="block w-full p-2 m-2 border"/>
+            <button className="bg-blue-500 text-white block w-full p-2 m-2 ">Register</button>
+            </form>
         </div>
     )
 }
