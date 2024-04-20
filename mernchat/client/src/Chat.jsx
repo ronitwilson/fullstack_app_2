e7738aab607ca14a58f3d001853a352f1482b801
Ronit Wilson <ronit.study@gmail.com>
2024-04-20 13:51:30 +0530

    intermediate commit

--- mernchat/client/src/Chat.jsx
+++ mernchat/client/src/Chat.jsx
@@ -80,6 +80,10 @@ export default function Chat() {
     }
         
     function sendMessage(ev) {
+        if(ev.target.type === 'file') {
+            console.log("file is ", ev.target.files[0])
+            return
+        }
         ev.preventDefault()
         console.log(" !!message is " ,message)
         ws.send(JSON.stringify({
@@ -90,6 +94,11 @@ export default function Chat() {
         setMessage('')
     }
 
+
+    function sendFile(ev) {
+        // console.log("file is ", ev.target.files[0])
+        sendMessage(ev)
+    }
 
     useEffect(( ) => {        
             const div = divUnderMessages.current;
@@ -179,11 +188,12 @@ export default function Chat() {
                     onChange={ev => {setMessage(ev.target.value)}}
                     placeholder="type a message" 
                     className="bg-white border p-2 flex-grow rounded-sm" />
-                    <button type="button" className="bg-blue-500 p-2 m-2 text-white rounded-sm">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                    <label type="button" className="bg-blue-500 p-2 m-2 cursor-pointer text-white rounded-sm">
+                    <input type="file" className="hidden" onChange={sendFile}/>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
                     </svg>
-                    </button>
+                    </label>
                     <button type="submit" className="bg-blue-500 p-2 m-2 text-white rounded-sm">Send</button>
                 </form>  
                 )}
